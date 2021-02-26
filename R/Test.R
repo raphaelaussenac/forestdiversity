5ce16fc99ce837bec5321c3843b41201aff3cc26
Arnaud Guyennon <arnaud.guyennon@irstea.fr>
2021-02-26 14:04:20 +0100

    Still in progress but now should work

--- R/Test.R
+++ R/Test.R
@@ -1,5 +1,5 @@
 TestPlotIndex <- function(){
-    N <- 40
+    N <- 20
     DF <- PlotExample
     DFgrid <- expand.grid(X=0:N, Y=0:N)
     DFgrid <- dplyr::mutate(DFgrid, species=sample(c('A', 'B', 'C'), (N+1)^2, replace=TRUE),
@@ -14,10 +14,15 @@ TestPlotIndex <- function(){
     DFgrid3 <- dplyr::mutate(DFgrid, species=rep(c('A', 'B'), ((N+2)^2)/2)[1:(N+1)^2])
     Td3 <- TabDist(DFgrid3, coord=c(0, N, 0, N))
 
-    print(c('random', 'regular','irregular'))
-    print(Compute_mingling(Td))
-    print(Compute_mingling(Td2))
-    print(Compute_mingling(Td3))
+    plot(Td)
+    print('Regular grid and random species / random dbh')
+    scan()
+    plot(Td2)
+    print('Regular grid and regular species / random dbh')
+    scan()
+    plot(Td3)
+    print('Regular grid and irregular species / random dbh')
+    scan()
 
     DFgrid2 <- dplyr::mutate(DFgrid, P=ceiling(3*(X+Y+1)/(2*(N+1))))
     DFgrid2 <- dplyr::mutate(DFgrid2, D_cm=c(10, 20, 30)[P])
@@ -26,10 +31,16 @@ TestPlotIndex <- function(){
     DFgrid3 <- dplyr::mutate(DFgrid, D_cm=rep(seq(10,60,length.out=2),
 	 ((N+2)^2)/2)[1:(N+1)^2])
     Td3 <- TabDist(DFgrid3, coord=c(0, N, 0, N))
-    print(c('random', 'regular','irregular'))
-    print(Compute_Size_Diff(Td))
-    print(Compute_Size_Diff(Td2))
-    print(Compute_Size_Diff(Td3))
+
+    plot(Td)
+    print('Regular grid and random species / random dbh')
+    scan()
+    plot(Td2)
+    print('Regular grid and random species / regular dbh')
+    scan()
+    plot(Td3)
+    print('Regular grid and random species / irregular dbh')
+    scan()
 
     DFgrid2 <- dplyr::mutate(DFgrid, X=runif((N+1)^2, 0, N),
         Y=runif((N+1)^2, 0, N))
@@ -39,10 +50,16 @@ TestPlotIndex <- function(){
         return(spatstat::runifdisc(n, radius, centre=c(x0, y0)))
     }
     PP <- spatstat::rPoissonCluster((N+5)^2/7, 0.2, nclust, radius=0.01, n=7)
-    DFgrid3 <- dplyr::mutate(DFgrid, X=40*PP$x[1:41^2], Y=40*PP$y[1:41^2])
+    DFgrid3 <- dplyr::mutate(DFgrid, X=N*PP$x[1:(N+1)^2], Y=N*PP$y[1:(N+1)^2])
     Td3 <- TabDist(DFgrid3, coord=c(0, N, 0, N))
-    print(c('grid', 'random','cluster'))
-    print(Compute_Winkelmass(Td))
-    print(Compute_Winkelmass(Td2))
-    print(Compute_Winkelmass(Td3))
+
+    plot(Td)
+    print('Regular grid')
+    scan()
+    plot(Td2)
+    print('Random positions')
+    scan()
+    plot(Td3)
+    print('Cluster positions')
+    scan()
 }
