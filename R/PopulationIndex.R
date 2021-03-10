10bb997eef1b67db47966d3d4b16910622311dd3
Arnaud Guyennon <arnaud.guyennon@irstea.fr>
2021-03-10 14:02:21 +0100

    Major changes to compute HillNb

--- R/PopulationIndex.R
+++ R/PopulationIndex.R
@@ -1,27 +1,3 @@
-#' Return Diversity metrics
-#'
-#' This function loads a according to the evalSite value.
-#' It return the diversity metrics for each year * site in a data.frame
-#' @param dataSet data.frame
-#' @param Nvar string, name of the diversity axis (D_cm for size, species for species)
-#' @param Inter int, width of each size class category
-#' @param path string, path of the data stored
-#' @return A data.frame of metrics
-#' @export
-ReturnDivIndex <- function(evalSite, Inter=10, path='DATA'){
-    if (!file.exists(paste0(path,'/','all_',evalSite,'.csv'))){stop('Need to build dataset first')}
-    dataTemp <- read.csv(file=paste0(path,'/','all_',evalSite,'.csv'))
-    DivIndexSize <- CalcDivIndex(dataTemp, Nvar="D_cm", Inter=Inter) %>%
-	    dplyr::mutate(ShSize=Sh, GSSize=GS, SimpSize=Simp, GiSize=GI, NClassSize=NClass) %>%
-	    dplyr::select(-Sh, -GS, -Simp, -GI, -NClass)
-    DivIndexSp <- CalcDivIndex(dataTemp, Nvar="species", Inter=Inter) %>%
-	    dplyr::mutate(ShSp=Sh, GSSp=GS, SimpSp=Simp, NSp=NClass) %>%
-	    dplyr::select(-Sh, -GS, -Simp, -GI, -NClass)
-    DivIndex <- dplyr::left_join(DivIndexSize, DivIndexSp, by=c('year','site','src'))
-    return(as.data.frame(DivIndex))
-}
-
-
 #' Compute diversity metrics
 #'
 #' This function compute the Gini index and Hill numbers for a population 
@@ -29,7 +5,8 @@ ReturnDivIndex <- function(evalSite, Inter=10, path='DATA'){
 #' @param Type string, define wether to compute proportion in terms of frequencies or relative basal area
 #' @return A data.frame containing diversity metrics for each site/year/source
 #' @export
-CalcDivIndex <- function(dataSet, Nvar = 'D_cm', Inter = 10, type = 'BA'){
+CalcDivIndex <- function(dataSet, Nvar = 'D_cm', Inter = 10, type = 'BA', Out='HillNb'){
+	if (!(Out %in% c('HillNb', 'Entropy'))){stop("Out should be 'HillNb' or 'Entropy'")}
     dataSet <- dplyr::mutate(dataSet, Var=dataSet[[Nvar]])
     if (Nvar %in% c('D_cm','H_m','V_m3')){
 	    dataSet <- dplyr::mutate(dataSet, Class = (1+floor((Var-Inter/2)/Inter))*Inter)
@@ -39,49 +16,55 @@ CalcDivIndex <- function(dataSet, Nvar = 'D_cm', Inter = 10, type = 'BA'){
 	    stop('Nvar specified not in output')
     }
     if (is.null(dataSet[["Var"]])){stop('Need to choose variable first')}
-    if (!('year' %in% names(dataSet))){dataSet <- dplyr::mutate(dataSet, year='NA')}
     if (!('site' %in% names(dataSet))){dataSet <- dplyr::mutate(dataSet, site='NA')}
     if (!('weight' %in% names(dataSet))){dataSet <- dplyr::mutate(dataSet, weight=1)}
+    listNameGrouping <- 'site'
+    if (('year' %in% names(dataSet))){listNameGrouping <- c(listNameGrouping, 'year')}
+    if (('src' %in% names(dataSet))){listNameGrouping <- c(listNameGrouping, 'src')}
+    if (('postThinning' %in% names(dataSet))){listNameGrouping <- c(listNameGrouping, 'postThinning')}
+    if (('postDisturbance' %in% names(dataSet))){listNameGrouping <- c(listNameGrouping, 'postDisturbance')}
     if (!('src' %in% names(dataSet))){dataSet <- dplyr::mutate(dataSet, src='NA')}
-    if (!('postThinning' %in% names(dataSet))){dataSet <- dplyr::mutate(dataSet, postThinning=FALSE)}
-    if (!('postDisturbance' %in% names(dataSet))){dataSet <- dplyr::mutate(dataSet, postDisturbance=FALSE)}
     dataSet <- dplyr::mutate(dataSet, BA=pi*(D_cm/200)^2*weight)
     dataSet <- data.table::as.data.table(dataSet)
     if (type=='BA'){
         if (Nvar!="species"){
 	     DivIndex <- dataSet[, .(Gini=GiniPop(Var, BA, weight),
-                 H=HillPop(Class, BA, OutFormat='str')), by=list(year, site, src, postThinning, postDisturbance)]
-             DivIndex <- dplyr::mutate(DivIndex, Sh=as.numeric(do.call(rbind, strsplit(H, '/'))[, 1]),
-                        GS=as.numeric(do.call(rbind, strsplit(H, '/'))[,2]),
-                        Simp=as.numeric(do.call(rbind, strsplit(H, '/'))[,3]),
-                        Nclass=as.numeric(do.call(rbind, strsplit(H, '/'))[,4]))
+                 H=DivPop(Class, BA, OutFormat='str', Out=Out)), by=listNameGrouping]
+             DivIndex <- dplyr::mutate(DivIndex, 
+                        Nclass=as.numeric(do.call(rbind, strsplit(H, '/'))[,1]),
+                        Sh=as.numeric(do.call(rbind, strsplit(H, '/'))[, 2]),
+                        Simp=as.numeric(do.call(rbind, strsplit(H, '/'))[, 3]))
 	     DivIndex <- dplyr::select(DivIndex, -H)
 
 	}else{
-             DivIndex <- dataSet[, .(H=HillPop(Class, BA, OutFormat='str')), by=list(year, site, src, postThinning, postDisturbance)]
-             DivIndex <- dplyr::mutate(DivIndex, Sh=as.numeric(do.call(rbind, strsplit(H, '/'))[, 1]),
-                        GS=as.numeric(do.call(rbind, strsplit(H, '/'))[,2]),
-                        Simp=as.numeric(do.call(rbind, strsplit(H, '/'))[,3]),
-                        Nclass=as.numeric(do.call(rbind, strsplit(H, '/'))[,4]))
+             DivIndex <- dataSet[, .(H=DivPop(Class, BA, OutFormat='str', Out=Out)), by=listNameGrouping]
+             DivIndex <- dplyr::mutate(DivIndex,  
+                        Nclass=as.numeric(do.call(rbind, strsplit(H, '/'))[,1]),
+                        Sh=as.numeric(do.call(rbind, strsplit(H, '/'))[, 2]),
+                        Simp=as.numeric(do.call(rbind, strsplit(H, '/'))[, 3]))
 	     DivIndex <- dplyr::select(DivIndex, -H)
 	}
     }else{
         if (Nvar!='species'){
 	     DivIndex <- dataSet[, .(Gini=GiniPop(Var, BA, weight),
-                 H=HillPop(Class, weight, OutFormat='str')), by=list(year, site, src, postThinning, postDisturbance)]
-             DivIndex <- dplyr::mutate(DivIndex, Sh=as.numeric(do.call(rbind, strsplit(H, '/'))[, 1]),
-                        GS=as.numeric(do.call(rbind, strsplit(H, '/'))[,2]),
-                        Simp=as.numeric(do.call(rbind, strsplit(H, '/'))[,3]),
-                        Nclass=as.numeric(do.call(rbind, strsplit(H, '/'))[,4]))
+                 H=DivPop(Class, weight, OutFormat='str', Out=Out)), by=listNameGrouping]
+             DivIndex <- dplyr::mutate(DivIndex,
+                        Nclass=as.numeric(do.call(rbind, strsplit(H, '/'))[,1]),
+                        Sh=as.numeric(do.call(rbind, strsplit(H, '/'))[, 2]),
+                        Simp=as.numeric(do.call(rbind, strsplit(H, '/'))[, 3]))
 	     DivIndex <- dplyr::select(DivIndex, -H)
 	}else{
-             DivIndex <- dataSet[, .(H=HillPop(Class, weight, OutFormat='str')), by=list(year, site, src, postThinning, postDisturbance)]
-             DivIndex <- dplyr::mutate(DivIndex, Sh=as.numeric(do.call(rbind, strsplit(H, '/'))[, 1]),
-                        GS=as.numeric(do.call(rbind, strsplit(H, '/'))[,2]),
-                        Simp=as.numeric(do.call(rbind, strsplit(H, '/'))[,3]),
-                        Nclass=as.numeric(do.call(rbind, strsplit(H, '/'))[,4]))
+             DivIndex <- dataSet[, .(H=DivPop(Class, weight, OutFormat='str', Out=Out)), by=listNameGrouping]
+             DivIndex <- dplyr::mutate(DivIndex,
+                        Nclass=as.numeric(do.call(rbind, strsplit(H, '/'))[,1]),
+                        Sh=as.numeric(do.call(rbind, strsplit(H, '/'))[, 2]),
+                        Simp=as.numeric(do.call(rbind, strsplit(H, '/'))[, 3]))
 	     DivIndex <- dplyr::select(DivIndex, -H)
 	}
+    }
+    if (Out=='HillNb'){
+        names(DivIndex)[which(names(DivIndex)=="Sh")] <- "H1"
+        names(DivIndex)[which(names(DivIndex)=="Simp")] <- "H2"
     }
     return(DivIndex)
 }
@@ -138,18 +121,49 @@ GiniPop <- function(Size, BA, weight = 1, PLOT=FALSE){
 #' @return The Hill Numbers index for the population
 #' @export
 HillPop <- function(Class, weight, OutFormat='list'){
-    P <- dplyr::group_by(data.frame(Class, weight), Class)
-    P <- dplyr::summarise(P, p = sum(weight))
-    P <- dplyr::ungroup(P)
-    P <- dplyr::mutate(P, p=p/sum(p))
-    Out <- data.frame(Sh=-sum(P$p*log(P$p)), GS=1-sum(P$p^2), Simp=sum(P$p^2), Nclass=dim(P)[1])
+    In <- EntropyPop(Class, weight, OutFormat='list')
+    Out <- data.frame(Nclass=In$Nclass, H1=exp(In$Sh), H2=1/(In$Simp))
     if (OutFormat=='list'){
         return(Out)
     }else if (OutFormat=='str'){
-        Out <- paste(Out$Sh, Out$GS, Out$Simp, Out$Nclass, sep='/') 
+        Out <- paste(Out$Nclass, Out$H1, Out$H2, sep='/') 
         return(Out)
     }else{
 	    stop('Outformat must be list or str')
     }
 }
 
+##################################################
+###################################################
+#' Compute Entropy indices
+#'
+#' This function compute the Hill numbers index for a population 
+#' @param Class numeric vector, class of each treee in the population (in cm)
+#' @param Weight, numeric vector, weight associated with each tree, can be the basal area
+#' @return The Hill Numbers index for the population
+#' @export
+EntropyPop <- function(Class, weight, OutFormat='list'){
+    P <- dplyr::group_by(data.frame(Class, weight), Class)
+    P <- dplyr::summarise(P, p = sum(weight))
+    P <- dplyr::ungroup(P)
+    P <- dplyr::mutate(P, p=p/sum(p))
+    Out <- data.frame(Sh=-sum(P$p*log(P$p)), Simp=sum(P$p^2), Nclass=dim(P)[1])
+    if (OutFormat=='list'){
+        return(Out)
+    }else if (OutFormat=='str'){
+        Out <- paste(Out$Nclass, Out$Sh, Out$Simp, sep='/') 
+        return(Out)
+    }else{
+	    stop('Outformat must be list or str')
+    }
+}
+
+##################################################
+###################################################
+#' Wrapper function for Div indices
+#'
+
+DivPop <- function(Class, weight, OutFormat, Out){
+    if (Out=='HillNb'){return(HillPop(Class=Class, weight=weight, OutFormat=OutFormat))}
+    if (Out=='Entropy'){return(EntropyPop(Class=Class, weight=weight, OutFormat=OutFormat))}
+}
