d36a2f163e4b9424c86e6421d1451f786cc86091
Arnaud Guyennon <arnaud.guyennon@irstea.fr>
2021-03-12 18:02:07 +0100

    major changes, normalization implemented

--- R/ResilienceIndex.R
+++ R/ResilienceIndex.R
@@ -7,22 +7,23 @@
 #' @return A data.frame containing resilience metrics for each simulation. A NA value is returned if there is an error
 #' -99 means that recovery was not complete and metrics could not be computed
 #' @export
-EventResilience <- function(dataSet, Nvar='V_m3', RecTime=20){
+EventResilience <- function(dataSet, Nvar='V_m3', RecTime=20, normalize='baseline'){
     if (!(Nvar %in% names(dataSet))){stop('Need a variable from dataSet')}
     if (!('VirtualExperiment' %in% class(dataSet))){stop('Need format first')}
     dataSetini <- dataSet[preDisturbance==TRUE, ]
-    dataSetini <- dplyr::select(dataSetini, -postThinning, -postDisturbance, -preDisturbance)
+    dataSetini <- dplyr::select(dataSetini, -postThinning, -postDisturbance, -preDisturbance, -YearDisturbance)
     dataSetini <- dataSetini[, lapply(.SD, mean), by=c('site','src')]
     names(dataSetini)[!(names(dataSetini) %in% c('year','site','src'))] <-
         paste0(names(dataSetini)[!(names(dataSetini) %in% c('year','site','src'))], 'ini')
     dataSet <- dplyr::mutate(dataSet, Var=dataSet[, ..Nvar][[1]])
-    TT <- dataSet[,.(RecMet=tryCatch(RecoveryMetrics(Var, year, preDisturbance, RecTime=RecTime, FormatOut='str'),
+    TT <- dataSet[,.(RecMet=tryCatch(RecoveryMetrics(Var, year, preDisturbance,
+        YearDisturbance, RecTime=RecTime, normalize=normalize, FormatOut='str'),
        error=function(e){return('-1/-1/-1')})), by='site']
-    TT <- dplyr::mutate(TT, Theta=as.numeric(do.call(rbind, strsplit(RecMet, '/'))[, 1]),
-	TimeRec=as.numeric(do.call(rbind, strsplit(RecMet, '/'))[,2]),
-	DegRec=as.numeric(do.call(rbind, strsplit(RecMet, '/'))[,3]),
-	Phi1=as.numeric(do.call(rbind, strsplit(RecMet, '/'))[,4]),
-	Phi2=as.numeric(do.call(rbind, strsplit(RecMet, '/'))[,5]))
+    TT <- dplyr::mutate(TT, TimeToRecover=as.numeric(do.call(rbind, strsplit(RecMet, '/'))[, 1]),
+	ThetaRecovery=as.numeric(do.call(rbind, strsplit(RecMet, '/'))[,2]),
+	DegreeRecovery=as.numeric(do.call(rbind, strsplit(RecMet, '/'))[,3]))
+#	Phi1=as.numeric(do.call(rbind, strsplit(RecMet, '/'))[,4]),
+#	Phi2=as.numeric(do.call(rbind, strsplit(RecMet, '/'))[,5]))
     TT <- dplyr::select(TT, -RecMet)
     TT[TT==-1] <- NA
     TT <- dplyr::mutate(TT, Nvar=Nvar)
@@ -40,30 +41,48 @@ EventResilience <- function(dataSet, Nvar='V_m3', RecTime=20){
 #' @param FormatOut, str, form of output
 #' @return resilience metrics in a list or a string
 #' @export
-RecoveryMetrics <- function(Var, Year, PreDisturb, RecTime=20, FormatOut='list'){
-    VarBef <- mean(Var[PreDisturb==TRUE])
-    YearBef <- max(Year[PreDisturb==TRUE])
-    if (length(VarBef)==0){stop('No value before perturbation')}
-    if (is.na(VarBef)){stop('Something wrong before perturbation')}
-    VarPost <- Var[PreDisturb==FALSE]
-    if (length(VarPost)==0){stop('No value after perturbation')}
-    Intens <- VarPost[1] / VarBef
-    if (Intens >= 1){warning('Perturbation did not reduce the variable')}
-    dT <- Year[which(Var >= VarBef & PreDisturb==FALSE)][1] - YearBef
-    idT <- which(Year>=(YearBef + RecTime))[1]
-    VardT <- Var[idT]
-    AdT <- VarBef * dT
-    NCI <- sum(abs(Var[1:idT][PreDisturb[1:idT]==FALSE]-VarBef))
-    Out <- data.frame(Theta=Intens/dT, TimeRec=dT,
-        DegRec=1-(VardT-VarBef)/(VarPost[1]-VarBef),
-       	Phi1=1 - NCI/AdT, Phi2=(VarBef - VarPost[1])*dT/NCI)
-    Out$DegRec[Out$DegRec>1] <- 1
-    if (is.na(dT)){Out$Theta <- -99;Out$TimeRec <- -99}
-    if (is.na(VardT)){Out$DegRec <- -99}
+RecoveryMetrics <- function(Var,
+			    Year,
+			    preDisturb,
+			    YearDisturbance,
+			    RecTime=20,
+			    normalize='baseline',
+			    FormatOut='list'){
+    com <- ''
+    YearDisturbance <- YearDisturbance[1]
+    Pd <- min(Var[preDisturb==FALSE])
+    C0 <- mean(Var[preDisturb==TRUE])
+    if (normalize=="baseline"){
+        Var <- Var/C0
+        C0 <- 1
+	Pd <- Pd/C0
+    }else if (normalize=="impact"){
+        dataSet <- dplyr::mutate(dataSet, Var= (Var-Pd)/(C0 - Pd))
+        C0 <- 1
+	Pd <- 0
+    }
+    Td <- Year[which(Var==min(Var[preDisturb==FALSE]))]
+    Tf <- Year[which(preDisturb==FALSE & Var>=mean(Var[preDisturb==TRUE]))][1]
+    T0 <- YearDisturbance[1]
+    P0 <- Var[preDisturb==FALSE & Year==min(Year[preDisturb==FALSE])]
+    Tx <- YearDisturbance[1] + RecTime
+    Px <- Var[which(Year>=YearDisturbance[1]+RecTime)[1]]
+    TimeToRecover <- Tf - YearDisturbance
+    DegreeRecovery <- Px / C0
+    ThetaRecovery <- (Px - C0) / (Tf - YearDisturbance)
+    AdT <- TimeToRecover * C0
+    if (max(Year) < (YearDisturbance + RecTime)){com <- 'RecTime is after the maximum year'; DegreeRecovery <- -99}
+    if (is.na(Tf)){com <- paste0(com, '/ No recovery'); TimeToRecover <- -99; ThetaRecovery <- -99}
+    if (sum(PreDisturb==FALSE)<1){com <- paste0(com, '/ No data after disturbance'); DegreeRecovery <- -99; ThetaRecovery <- -99; TimeToRecover <- -99}
+    if (C0 < Pd){com <- paste0(com, '/ Perturbation did not reduce the variable')}
+    if (is.na(C0)){com <- paste0(com, '/ No data before perturbation'); DegreeRecovery <- -99; ThetaRecovery <- -99; TimeToRecover <- -99}
+    if (max(Year) < (YearDisturbance + RecTime)){com <- 'RecTime is after the maximum year'; DegreeRecovery <- -99}
+    Out <- data.frame(TimeToRecover=TimeToRecover, DegreeRecovery=DegreeRecovery,
+        ThetaRecovery=ThetaRecovery, RecTime=RecTime, com=com)
     if (FormatOut=='list'){
 	return(Out)
     }else if (FormatOut=='str'){
-	return(paste(Out$Theta, Out$TimeRec, Out$DegRec, Out$Phi1, Out$Phi2, sep='/'))
+	return(paste(Out$TimeToRecover, Out$DegreeRecovery, Out$ThetaRecovery, sep='/'))
     }else{
         stop('FormatOut should be list or str')
     }
@@ -110,12 +129,11 @@ format_samsara_Pop <- function(dataRaw){
 #' @return dataSet, data.table, formatted outuput
 #' @export
 format_salem <- function(dataRaw, ClassInter=10, ClassIni=7.5, Out='HillNb', type='BA'){
-    listNameGrouping <- c('year', 'site', 'src', 'postThinning', 'postDisturbance')
-    if (!('year' %in% names(dataRaw))){stop('Need at least severak years')}
-    if (!('site' %in% names(dataRaw))){dataRaw <- dplyr::mutate(dataRaw, site='NA')}
+    listNameGrouping <- c('site', 'src')
+    if (('year' %in% names(dataRaw))){listNameGrouping <- c(listNameGrouping, 'year')}
+    if (('postThinning' %in% names(dataRaw))){listNameGrouping <- c(listNameGrouping, 'postThinning')}
+    if (('postDisturbance' %in% names(dataRaw))){listNameGrouping <- c(listNameGrouping, 'postDisturbance')}
     if (!('src' %in% names(dataRaw))){dataRaw <- dplyr::mutate(dataRaw, src='salem')}
-    if (!('postThinning' %in% names(dataRaw))){dataRaw <- dplyr::mutate(dataRaw, postThinning=FALSE)}
-    if (!('postDisturbance' %in% names(dataRaw))){stop('Need logical variable postDisturbance')}
     dataRaw <- data.table::as.data.table(dataRaw)
     dataRaw$postThinning <- as.logical(dataRaw$postThinning)
     dataRaw$postDisturbance <- as.logical(dataRaw$postDisturbance)
@@ -126,13 +144,18 @@ format_salem <- function(dataRaw, ClassInter=10, ClassIni=7.5, Out='HillNb', typ
     names(HetIndexSp)[!(names(HetIndexSp) %in% listNameGrouping)] <-
          paste0(names(HetIndexSp)[!(names(HetIndexSp) %in% listNameGrouping)], 'Sp')
     dataSet <- dataRaw[, .(V_m3=sum(weight*V_m3), Dg=sqrt(sum(weight*D_cm^2)/sum(weight)),
-	 N=sum(weight), BA=pi*sum(weight*(D_cm/200)^2)), by=listNameGrouping]
+         N=sum(weight), BA=pi*sum(weight*(D_cm/200)^2)), by=listNameGrouping]
     dataSet <- merge(dataSet, HetIndexSize, by=listNameGrouping, all.x=TRUE)
     dataSet <- merge(dataSet, HetIndexSp, by=listNameGrouping, all.x=TRUE)
-    dataSet <- dplyr::mutate(dataSet, BAinc=c(diff(BA), NA))
+    dataSet <- dataSet[, BAinc:=c(diff(BA), NA), by=listNameGrouping]
+    if (sum(dataSet$postDisturbance==TRUE)==0){
+        dataSet <- dplyr::mutate(dataSet, YearDisturbance=NA, preDisturbance=FALSE, CONTROL=TRUE)
+    }else{
+        dataSet <- dataSet[, YearDisturbance:=min(year[postDisturbance==TRUE]), by=list(site, src)]
+        dataSet <- dplyr::mutate(dataSet, preDisturbance=(year<=YearDisturbance & postDisturbance==FALSE),
+	    CONTROL=FALSE)
+    }
     class(dataSet) <- append('VirtualExperiment', class(dataSet))
-    dataSet <- dplyr::mutate(dataSet, preDisturbance=FALSE)
-    dataSet <- dataSet[, preDisturbance:=(year<=min(year) & postDisturbance==FALSE), by=list(site, src)]
     return(dataSet)
 }
 
@@ -143,42 +166,109 @@ format_salem <- function(dataRaw, ClassInter=10, ClassIni=7.5, Out='HillNb', typ
 #' @param Nvar, string, dataSet, variable to be plotted
 #' @return plot Variable chosen
 #' @export
-plot.VirtualExperiment <- function(dataSet, Nvar='BA', RecTime=20){
+plot.VirtualExperiment <- function(dataSet, Nvar='BA', RecTime=20, normalize='baseline'){
     if (!(Nvar %in% names(dataSet))){stop('Need a variable from dataSet')}
     if (!('VirtualExperiment' %in% class(dataSet))){stop('Need format first')}
     dataSet <- dplyr::mutate(dataSet, Var=dataSet[, ..Nvar][[1]])
-    YearBef <- max(dataSet$year[dataSet$preDisturb==TRUE])
-    VarBef <- mean(dataSet$Var[dataSet$preDisturbance==TRUE])
-    Varmin <- min(dataSet$Var)
-    Pd <- dataSet$Var[dataSet$year==min(dataSet$year[dataSet$preDisturb==FALSE]) & dataSet$preDisturb==FALSE]
-    Td <- min(dataSet$year[dataSet$preDisturb==FALSE])
-    Px <- dataSet$Var[dataSet$year==Td + RecTime]
-    E <- EventResilience(dataSet, Nvar=Nvar, RecTime=RecTime)
-    print(E$Phi1)
-    print(E$Phi2)
-    pl <- ggplot2::ggplot(dataSet, ggplot2::aes(x=year, y=Var)) +
-       	ggplot2::ggtitle(paste0('Variable : ', Nvar, ' / Model : ', dataSet$src[1],
-		'  Phi1 : ', round(E$Phi1, digits=2), '  Phi2 : ', round(E$Phi2,digits=2))) +
-	ggplot2::theme_bw(base_size=16) + ggplot2::ylab('') +
-	geom_hline(aes(yintercept=VarBef), linetype=2,col='blue') +
-	geom_vline(aes(xintercept=E$TimeRec + YearBef), linetype=2) +
-	geom_vline(aes(xintercept=YearBef + RecTime), linetype=2) +
-	geom_text(aes(y=0, x=1+YearBef + E$TimeRec, label='Tf'), size=10, col='black') +
-	geom_text(aes(y=0, x=1+YearBef + RecTime, label='Tx'), size=10, col='black') +
-	geom_text(aes(y=VarBef*1.05, x=YearBef + 1, label='Y0=Yd'), size=10, col='blue') +
-	geom_text(aes(y=Pd*0.95, x=Td, label='Pd'), size=10, col='blue') +
-	geom_text(aes(y=0, x=Td, label='Td'), size=10, col='black') +
-	geom_text(aes(y=Px*0.95, x=1+YearBef+RecTime, label='Px'), size=10, col='black') +
-	geom_vline(aes(xintercept=Td), linetype=2, col='black') +
-	coord_cartesian(ylim=c(0, max(dataSet$Var))) +
-	ggplot2::geom_point(ggplot2::aes(col=preDisturbance)) + ggplot2::facet_wrap(~site, scales='free') +
-       	ggplot2::geom_line(col='black') +
-	geom_ribbon(data=dplyr::filter(dataSet, year<=(YearBef + RecTime)), aes(ymin=VarBef, ymax=Var), alpha=0.4) +
-	geom_ribbon(data=dplyr::filter(dataSet, year<=(YearBef+E$TimeRec)), aes(ymin=0, ymax=VarBef), alpha=0.2, fill='red')
+    dataSet <- data.table::setDT(dataSet)
+    if (dataSet$CONTROL[1]==FALSE){
+        dataSet <- dataSet[, C0:=mean(Var[preDisturbance==TRUE]), by=list(site, src)]
+        dataSet <- dataSet[, Pd:=min(Var[preDisturbance==FALSE]), by=list(site, src)]
+        if (normalize=='baseline'){
+	    dataSet <- dplyr::mutate(dataSet, Var=Var/C0)
+	}else if (normalize=='impact'){
+	    dataSet <- dplyr::mutate(dataSet, Var= (Var-Pd)/(C0 - Pd))
+	}
+        Points <- dataSet[, .(C0=mean(Var[preDisturbance==TRUE]),  # Mean value of variable  before perturbation
+            Pd=min(Var[preDisturbance==FALSE]),
+            Td=year[which(Var==min(Var[preDisturbance==FALSE]))],  # Minimum value an time of perturbed 
+            Tf=year[which(preDisturbance==FALSE & Var>=mean(Var[preDisturbance==TRUE]))][1], # First year of recovery of initial value
+	    T0=YearDisturbance[1],
+       	    P0=Var[preDisturbance==FALSE & year==min(year[preDisturbance==FALSE])],
+	    Tx=YearDisturbance[1]+RecTime,
+            Px=Var[which(year>=YearDisturbance[1]+RecTime)[1]]), by=list(site, src)]
+        Area <- merge(dplyr::select(dataSet, -C0), Points, by=c("site", "src"), all.x=TRUE)
+        pl <- ggplot2::ggplot(data=Points) + ggplot2::geom_hline(ggplot2::aes(yintercept=C0), linetype=2, col='blue') +
+	ggplot2::geom_vline(ggplot2::aes(xintercept=Tf), linetype=2) +
+	ggplot2::geom_vline(ggplot2::aes(xintercept=Tx), linetype=2) + 
+	ggplot2::geom_vline(ggplot2::aes(xintercept=Td), linetype=2) +
+	ggplot2::geom_text(ggplot2::aes(y=0, x=1+Tf, label='Tf'), size=10) +
+	ggplot2::geom_text(ggplot2::aes(y=0, x=1+Tx, label='Tx'), size=10) +
+	ggplot2::geom_text(ggplot2::aes(y=0, x=Td, label='Td'), size=10) +
+	ggplot2::geom_text(ggplot2::aes(y=C0*1.05, x=1+Td, label='Y0=Yd'), size=10) +
+	ggplot2::geom_text(ggplot2::aes(y=Pd*0.95, x=1+Td, label='Pd'), size=10) +
+	ggplot2::geom_text(ggplot2::aes(y=Px*0.95, x=1+Tx, label='Px'), size=10) +
+	ggplot2::geom_point(data=dataSet, ggplot2::aes(x=year, y=Var, col=preDisturbance), size=2) +
+	ggplot2::geom_line(data=dataSet, ggplot2::aes(x=year, y=Var)) +
+	ggplot2::geom_ribbon(data=dplyr::filter(Area, year<=Tf, year>=T0), ggplot2::aes(x=year, ymin=C0, ymax=Var), alpha=0.7, fill='yellow') +
+	ggplot2::geom_ribbon(data=dplyr::filter(Area, year>=Tf,year<=Tx), ggplot2::aes(x=year, ymin=C0, ymax=Var), alpha=0.4, fill='yellow') +
+	ggplot2::geom_ribbon(data=dplyr::filter(Area, year<=Tf, year>=T0), ggplot2::aes(x=year, ymin=0*C0, ymax=C0), alpha=0.2, fill='red') +
+	ggplot2::scale_color_manual(values=c('red', 'blue')) +
+	ggplot2::facet_wrap(~site, scales='free') + ggplot2::theme_bw(base_size=16)
+    }else if(dataSet$CONTROL[1]==TRUE){
+        pl <- ggplot2::ggplot(data=dataSet, ggplot2::aes(x=year,y=Var, size=2)) +
+	    ggplot2::geom_line() +
+	    ggplot2::facet_wrap(~site, scales='free') + ggplot2::theme_bw(base_size=16)
+    }
     print(pl)
 }
 
+buildExpControl <- function(DFexp, DFcontrol){
+   DFEXP <- format_salem(DFexp)
+   DFCONTROL <- format_salem(DFcontrol)
+   DF <- rbind(DFEXP, DFCONTROL)
+   class(dataSet) <- append('VirtualExperimentControl', class(dataSet))
+}
 
+#' Plot virtual experiment
+#'
+#' This function plot a variable from formatted dataSet
+#' @param dataSet, formatted dataset (VirtualExperiment object)
+#' @param Nvar, string, dataSet, variable to be plotted
+#' @return plot Variable chosen
+#' @export
+plot.VirtualExperimentControl <- function(dataSet, Nvar='V_m3', RecTime=20, normalize='baseline'){
+    if (!(Nvar %in% names(dataSet))){stop('Need a variable from dataSet')}
+    if (!('VirtualExperimentControl' %in% class(dataSet))){stop('Need format first')}
+    dataSet <- dplyr::mutate(dataSet, Var=dataSet[, ..Nvar][[1]])
+    if (dataSet$CONTROL[1]==FALSE){
+        dataSet <- dataSet[, C0:=mean(Var[preDisturbance==TRUE],
+            Pd=min(Var[preDisturbance==FALSE])), by=list(site, src)]
+        if (normalize=='baseline'){dataSet <- dplyr::mutate(dataSet, Var=Var/C0)}
+        Points <- dataSet[, .(C0=mean(Var[preDisturbance==TRUE]),  # Mean value of variable  before perturbation
+            Pd=min(Var[preDisturbance==FALSE]),
+            Td=year[which(Var==min(Var[preDisturbance==FALSE]))],  # Minimum value an time of perturbed 
+            Tf=year[which(preDisturbance==FALSE & Var>=mean(Var[preDisturbance==TRUE]))][1], # First year of recovery of initial value
+	    T0=YearDisturbance[1],
+       	    P0=Var[preDisturbance==FALSE & year==min(year[preDisturbance==FALSE])],
+	    Tx=YearDisturbance[1]+RecTime,
+            Px=Var[which(year>=YearDisturbance[1]+RecTime)[1]]), by=list(site, src)]
+
+        Area <- merge(dplyr::select(dataSet, -C0), Points, by=c("site", "src"), all.x=TRUE)
+        pl <- ggplot2::ggplot(data=Points) + ggplot2::geom_hline(ggplot2::aes(yintercept=C0), linetype=2, col='blue') +
+	ggplot2::geom_vline(ggplot2::aes(xintercept=Tf), linetype=2) +
+	ggplot2::geom_vline(ggplot2::aes(xintercept=Tx), linetype=2) + 
+	ggplot2::geom_vline(ggplot2::aes(xintercept=Td), linetype=2) +
+	ggplot2::geom_text(ggplot2::aes(y=0, x=1+Tf, label='Tf'), size=10) +
+	ggplot2::geom_text(ggplot2::aes(y=0, x=1+Tx, label='Tx'), size=10) +
+	ggplot2::geom_text(ggplot2::aes(y=0, x=Td, label='Td'), size=10) +
+	ggplot2::geom_text(ggplot2::aes(y=C0*1.05, x=1+Td, label='Y0=Yd'), size=10) +
+	ggplot2::geom_text(ggplot2::aes(y=Pd*0.95, x=1+Td, label='Pd'), size=10) +
+	ggplot2::geom_text(ggplot2::aes(y=Px*0.95, x=1+Tx, label='Px'), size=10) +
+	ggplot2::geom_point(data=dataSet, ggplot2::aes(x=year, y=Var, col=preDisturbance), size=2) +
+	ggplot2::geom_line(data=dataSet, ggplot2::aes(x=year, y=Var)) +
+	ggplot2::geom_ribbon(data=dplyr::filter(Area, year<=Tf, year>=T0), ggplot2::aes(x=year, ymin=C0, ymax=Var), alpha=0.7, fill='yellow') +
+	ggplot2::geom_ribbon(data=dplyr::filter(Area, year>=Tf, year<=Tx), ggplot2::aes(x=year, ymin=C0, ymax=Var), alpha=0.4, fill='yellow') +
+	ggplot2::geom_ribbon(data=dplyr::filter(Area, year<=Tf, year>=T0), ggplot2::aes(x=year, ymin=0*C0, ymax=C0), alpha=0.2, fill='red') +
+	ggplot2::scale_color_manual(values=c('red', 'blue')) +
+	ggplot2::facet_wrap(~site, scales='free') + ggplot2::theme_bw(base_size=16)
+    }else if(dataSet$CONTROL[1]==TRUE){
+        pl <- ggplot2::ggplot(data=dataSet, ggplot2::aes(x=year,y=Var, size=2)) +
+	    ggplot2::geom_line() +
+	    ggplot2::facet_wrap(~site, scales='free') + ggplot2::theme_bw(base_size=16)
+    }
+    print(pl)
+}
 
 
 
