cb1aa4e6ef58fa41f0cfdf888b38b97fd6dd0dac
Arnaud Guyennon <arnaud.guyennon@irstea.fr>
2021-04-08 18:26:23 +0200

    first commit

---
+++ R/LandscapeExample.R
@@ -0,0 +1,13 @@
+#' Landscape distribution data
+#'
+#' Data from a virtual landscape, each line representing a cell with tree distribution by species
+#'
+#' @docType data
+#'
+#' @usage data(LandscapeExample)
+#'
+#' @format An object of class data.frame
+#'
+#' @keywords datasets
+#'
+#' @examples
